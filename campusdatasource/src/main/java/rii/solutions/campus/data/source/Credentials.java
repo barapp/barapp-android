00efcc2ce0ea0a1810e224943d4bf15da8bde9a4
Igor Yanishevskiy <igorianishevsky@gmail.com>
2017-10-27 18:16:29 +0200

    Hidden credentials (see gradle-buildconfig-plugin, gradle-credentials-plugin)

--- campusdatasource/src/main/java/rii/solutions/campus/data/source/Credentials.java
+++ campusdatasource/src/main/java/rii/solutions/campus/data/source/Credentials.java
@@ -1,13 +1,13 @@
 package rii.solutions.campus.data.source;
 
+import rii.solutions.campus.data.BuildConfig;
+
 /**
- * TODO this must be hidden!
- *
  * Created by mejmo on 23.5.2017.
  */
 
 public interface Credentials {
-    String API_USERNAME = "barapp";
-    String API_PASS = "k238sdfaaslsad";
-    String API_BASE_URL = "http://develmagic.com:8000";
+    String API_USERNAME = BuildConfig.API_USERNAME;
+    String API_PASS = BuildConfig.API_PASS;
+    String API_BASE_URL = BuildConfig.API_BASE_URL;
 }
